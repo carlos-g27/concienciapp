c5134a95b95a1dc7c333ac8ae84cc2d62ec865fe
Carlos Alvarez <c.carlosalvarez.66@gmail.com>
2026-06-10 14:33:23 -0500

    weird change to home

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,8 @@ export default function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
+            <h2 className="font-medium text-xl mb-4">Pagina en construcción</h2>
+            <h2>🙈🙈🙈🙈🙈🙈🙈🙈🙈🙈</h2>
           </main>
         </div>
 
