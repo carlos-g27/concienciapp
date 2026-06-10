4af7b5428816f6aeb876a71521812ae6a7d7a6ee
Carlos Alvarez <c.carlosalvarez.66@gmail.com>
2026-06-10 14:11:36 -0500

    fixed sidebar component

--- components/settings-component/settings.tsx
+++ components/settings-component/settings.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import SidebarVisivility from "../../components/dashboard-logic/sidebar-config";
+import SidebarLayout from "../../components/dashboard-logic/sidebar-config";
 
 export default function Settings() {
-    return(
-        <>
-            <SidebarVisivility  />
-        </>
-    )
+  return (
+    <SidebarLayout pageTitle="Configuración">
+        <></>
+    </SidebarLayout>
+  );
 }
