18c4b0d6ea2d51033ae0b5eaa1702eaedae4a05e
Carlos Alvarez <c.carlosalvarez.66@gmail.com>
2026-06-10 15:00:41 -0500

    fixed login form in mobiles

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,6 @@ export default function Home() {
               </Suspense>
             )}
           </div>
-          <LoginForm />
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <main className="flex-1 flex flex-col gap-6 px-4">
@@ -36,6 +35,7 @@ export default function Home() {
             <h2>🙈🙈🙈🙈🙈🙈🙈🙈🙈🙈</h2>
           </main>
         </div>
+        <LoginForm />
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
--- components/settings-component/settings.tsx
+++ components/settings-component/settings.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import SidebarLayout from "../../components/dashboard-logic/sidebar-config";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 
 export default function Settings() {
   return (
     <SidebarLayout pageTitle="Configuración">
-        <></>
+        <ThemeSwitcher />
     </SidebarLayout>
   );
 }
